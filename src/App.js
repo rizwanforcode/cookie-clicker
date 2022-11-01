20dcb6edbe40db5b0c6b879f692d7dd192e18779
Rizwan Ahmad <rizwanforcode@gmail.com>
2022-11-01 22:52:40 +0545

    Added the save to localStorage with the use of useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Cookie from "./Cookie";
 import Count from "./Count";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(
+    JSON.parse(localStorage.getItem("cookieClickedCount") || 0)
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cookieClickedCount", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div className="container">
       <Cookie count={count} setCount={setCount} />
