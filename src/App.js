c1f9b0a3089840aef70eabc28ec646eba811ec47
Rizwan Ahmad <rizwanforcode@gmail.com>
2022-11-01 22:02:29 +0545

    Created the cookie right in the appjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import cookiePic from "./cookiePic.png";
 
 function App() {
+  const count = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={cookiePic} alt="cookie" />
+      <p className="count">{count}</p>
     </div>
   );
 }
